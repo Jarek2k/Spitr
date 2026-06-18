ea50b57376f204ca3257195a39d6e25bb72b5996
Jarek <jarekgster@gmail.com>
2026-06-18 21:04:12 +0200

    Initial Commit

---
+++ Spitr/SpitrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpitrApp.swift
+//  Spitr
+//
+//  Created by Jarek Gustak on 18.06.26.
+//
+
+import SwiftUI
+
+@main
+struct SpitrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
